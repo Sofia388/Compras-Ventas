f1433734566b14472c28e903e380db8c1ee405d5
ethan10rodriguez <62311552+ethan10rodriguez@users.noreply.github.com>
2021-09-21 08:13:41 -0600

    Add files via upload

--- Model/Conexion.Model.php
+++ Model/Conexion.Model.php
@@ -3,10 +3,10 @@
     {
         function __construct()
         {
-        $servidor="localhost";
-        $user="root";
-        $pass="";
-        $BaseDatos="ComprasVentas";
+        $servidor="sql208.byethost10.com";
+        $user="b10_27746189";
+        $pass="XJLQkA_F8i5M9";
+        $BaseDatos="b10_27746189_ComprasVentas";
 
         parent:: __construct($servidor,$user,$pass,$BaseDatos);
         $this->query("SET NAMES 'utf8';");
--- Model/Usuario.Model.php
+++ Model/Usuario.Model.php
@@ -1,22 +1,22 @@
 <?php
 
- class Usuarios
+ class Usuario
  {
  public function __construct()
         {
             $this->con=new Conexion();
         }
             
-        public function BuscarUser($numUs, $pass)
+        public function BuscarUser($num, $pass)
         {
             $this->con=new Conexion();
-            $sql="SELECT * FROM `trabajador` WHERE `Depto_idDepto` = '$numUs' AND `Contrasena`='$pass';";
+            $sql="SELECT * FROM `trabajador` WHERE `Depto_idDepto` = '$num' AND `Contrasena`='$pass';";
             $consulta=$this->con->query($sql);
             $this->con->close();
             return $consulta;
         }
 
-        public function GuardarUser($correo,$nombre,$apellido,$contrasena,$depto_idDepto,)
+        public function GuardarUsuario($correo,$nombre,$apellido,$contrasena,$depto_idDepto)
             {
                 $this->con=new Conexion();
                 $sql="INSERT INTO `trabajador`(`idUsuario`, `Depto_idDepto`, `Estado_idEstado`, `Roles_idRoles`, `Nombre`, `Apellido`,`Correo`) VALUES ('$correo','$nombre','$apellido','$contrasena','$dDepto_idDepto');"; 
